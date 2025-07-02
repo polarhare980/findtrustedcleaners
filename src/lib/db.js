228f7d03a1ce17183293992ff0a2747d76557c88
mat <matjoneshare@gmail.com>
2025-07-02 11:08:51 -0700

    client save fix mk3

--- src/lib/db.js
+++ src/lib/db.js
@@ -16,15 +16,12 @@ export async function connectToDatabase() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((mongoose) => mongoose)
-    .catch((err) => {
-      console.error('MongoDB connection error:', err);
-      throw err;  // Rethrow the error after logging it
-    });
+    cached.promise = mongoose.connect(MONGODB_URI)
+      .then((mongoose) => mongoose)
+      .catch((err) => {
+        console.error('MongoDB connection error:', err);
+        throw err;  // Rethrow the error after logging it
+      });
   }
 
   cached.conn = await cached.promise;
