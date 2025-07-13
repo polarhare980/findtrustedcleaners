cd3c55434e58050171d8b650e1d41902adabc243
mat <matjoneshare@gmail.com>
2025-07-13 07:48:34 -0700

    fixes/updates

--- src/app/dashboard/client/page.jsx
+++ src/app/dashboard/client/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import ClientDashboardComponent from '@/app/clients/[id]/clientdashboardcomponent';
+import ClientDashboardComponent from '@/app/clients/[id]/ClientDashboardComponent';
 
 export default function ClientDashboardPage() {
   return <ClientDashboardComponent />;
