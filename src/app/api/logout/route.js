6254211713499581661acd7b3cd835b3be2eb3db
mat <matjoneshare@gmail.com>
2025-07-24 08:57:43 -0700

    cleint registartion page update

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -2,12 +2,14 @@ import { serialize } from 'cookie';
 
 export async function POST() {
   const cookie = serialize('token', '', {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
-    path: '/',
-    expires: new Date(0), // ✅ Instantly expires the cookie
-  });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'lax',
+  path: '/',
+  expires: new Date(0),
+  maxAge: 0,
+});
+
 
   return new Response(JSON.stringify({ success: true }), {
     status: 200,
