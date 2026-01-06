6c5582bf1ac17a56616c537f87a3b75553c7ea6a
mat <matjoneshare@gmail.com>
2026-01-06 04:17:22 -0800

    Fix client dashboard import path/casing

---
+++ src/components/ClientDashboardComponent.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+// CHANGE THIS PATH to wherever your real client dashboard component actually lives:
+import RealClientDashboard from '@/components/client/ClientDashboard'; 
+
+export default function ClientDashboardComponent(props) {
+  return <RealClientDashboard {...props} />;
+}
