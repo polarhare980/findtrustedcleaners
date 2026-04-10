ada3faaca0be3b1ca8631214fa3039b17361ae26
mat <matjoneshare@gmail.com>
2026-04-10 11:50:05 -0700

    Fix JSX comment and unclosed p tag

--- src/components/PublicHeader.jsx
+++ src/components/PublicHeader.jsx
@@ -37,7 +37,7 @@ export default function PublicHeader() {
           <img
             src="/findtrusted-logo.png"
             alt="Find Trusted Cleaners"
-            className="h-12 w-auto sm:h-14"
+            className="h-14 w-auto sm:h-16"
           />
         </Link>
 
